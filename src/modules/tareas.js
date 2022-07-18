80d5ec1e0ca03fbb5e0894bfc777ba6d587ec12c
Gerson Calvo <gedacaba@gmail.com>
2022-07-18 10:56:21 -0500

    primera configuración

---
+++ src/modules/tareas.js
@@ -0,0 +1,15 @@
+export default {
+  namespaced: true,
+  state: {
+    tareasState: ['tarea 1', 'tarea 2', 'tarea 3'],
+  },
+  mutations:{
+
+  },
+  actions:{
+
+  },
+  getters:{
+
+  }
+}
---
+++ src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2 v-for="(item, index) in tareasState" :key="index">{{item}}</h2>
+    <h3>{{contador}}</h3>
+    <button @click="aumetarContador(1)">+</button>
+    <h3>{{titulo}}</h3>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+export default {
+  computed:{
+    ...mapState('tareas', ['tareasState']),
+    ...mapState(['titulo']),
+    ...mapState('contador', ['contador'])
+  },
+  methods:{
+    ...mapMutations('contador', ['aumetarContador'])
+  }
+}
+</script>
